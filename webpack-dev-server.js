af3607cf362ee5f1c1761eed375166859545707f
dbelyaev <dbelyaev@geeksforless.net>
2015-10-13 17:52:09 +0300

    s

---
+++ webpack-dev-server.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+//config.entry.app.unshift("webpack-dev-server/client?http://localhost:3000", "webpack/hot/dev-server");
+
+var compiler = webpack(config);
+
+var server = new WebpackDevServer(compiler, {
+    contentBase: __dirname + '/',
+    publicPath: '/HMR_path/',
+    colors: true,
+    hot: true,
+    inline: true,
+}).listen(3000, 'localhost');
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,25 @@
-var path = require('path');
 var webpack = require('webpack');
-var BowerWebpackPlugin = require('bower-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: './index',
+    entry: [
+        'webpack/hot/dev-server',
+        "./js/app.js"
+    ],
     output: {
-        path: path.resolve(__dirname, 'build'),
-        //publicPath: '/assets/',
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx'],
+        path: __dirname + '/build',
+        publicPath: '/HMR_path/',
+        filename: "bundle.js",
+        //chunkFilename: "[id].bundle.js"
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
-            {test: /\.json$/, loader: 'json-loader'},
-            {test: /\.hbs$/, loader: 'handlebars-loader'},
-            {test: /\.jsx$/, loader: 'jsx-loader'},
-            {test: /\.jsx$/, loader: 'jsx-loader'},
-            {
-                test: /\.(eot|woff|ttf|svg|png|jpg)$/,
-                loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
-            }
+            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.css$/, loader: "style!css" }
         ]
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
         new webpack.HotModuleReplacementPlugin(),
-        new ExtractTextPlugin("styles.css"),
-        new webpack.DefinePlugin({
-            'NODE_ENV': JSON.stringify('development')
-        }),
-        new BowerWebpackPlugin({
-            modulesDirectories: ['bower_components'],
-            manifestFiles: ['bower.json', '.bower.json'],
-            includes: /.*/,
-            excludes: /.*\.less$/
-        })
+        new webpack.NoErrorsPlugin()
     ]
+
 };
