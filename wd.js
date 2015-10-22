957c6f3791a369b6cb875c9838650a82abbdb295
dbelyaev <dbelyaev@geeksforless.net>
2015-10-22 18:33:24 +0300

    2

--- wd.js
+++ wd.js
@@ -16,17 +16,19 @@ profile.addExtension(path.resolve(__dirname, 'extensions/modify_headers.xpi'), f
 
     profile.setNativeEventsEnabled(false);
 
-    profile.setPreference("general.useragent.override", "User Agent string to force application/vnd.wap.xhtml+xml content..");
+    profile.setPreference("general.useragent.override", "some UA string");
 
-    profile.setPreference("general.headers.X-Forwarded-For", "1.2.3.4");
-
-    profile.setPreference("modifyheaders.headers.count", 1);
+    profile.setPreference("modifyheaders.config.active", true);
+    profile.setPreference("modifyheaders.config.alwaysOn", true);
+    profile.setPreference("modifyheaders.headers.count", 2);
     profile.setPreference("modifyheaders.headers.action0", "Add");
     profile.setPreference("modifyheaders.headers.name0", "X-Forwarded-For");
     profile.setPreference("modifyheaders.headers.value0", "1.2.3.4");
     profile.setPreference("modifyheaders.headers.enabled0", true);
-    profile.setPreference("modifyheaders.config.active", true);
-    profile.setPreference("modifyheaders.config.alwaysOn", true);
+    profile.setPreference("modifyheaders.headers.action1", "Add");
+    profile.setPreference("modifyheaders.headers.name1", "X-Forwarded-For");
+    profile.setPreference("modifyheaders.headers.value1", "1.2.3.5");
+    profile.setPreference("modifyheaders.headers.enabled1", true);
 
     //profile.setProxy({
     //    proxyType: 'manual',
@@ -43,10 +45,15 @@ profile.addExtension(path.resolve(__dirname, 'extensions/modify_headers.xpi'), f
             //.setFirefoxOptions(/* ... */)
             .build();
 
-        driver.get('http://www.seogadget.ru/myip');
+        //???????? IP ??????? ??????????: 193.46.86.0 - 193.46.86.255
+
+        //driver.get('http://www.seogadget.ru/myip');
+        driver.get('http://rassanov.ru/web-tools/my-IP.htm');
+
+
         //driver.findElement(By.name('q')).sendKeys('webdriver');
         //driver.findElement(By.name('btnG')).click();
-        driver.wait(until.titleIs('webdriver - Google Search'), 100000);
+        driver.wait(until.titleIs('webdriver - Google Search'), 30000);
         driver.quit();
     });
 });
