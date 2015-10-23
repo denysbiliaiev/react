cd38b2aa574691ebe1eaaf9aa22b0019f9d03834
dbelyaev <dbelyaev@geeksforless.net>
2015-10-23 17:57:23 +0300

    end

--- js/components/Client.jsx
+++ js/components/Client.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ImgC from 'react-image-component';
+import data from '../../json/data.json';
 
 let Client = React.createClass({
 
     render() {
         return(
             <div>
-                <h1>Client</h1>
+                <h1>{data.name}</h1>
                 <ImgC src={"react.png"} aspectRatio={0.1} />
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: ['webpack/hot/dev-server', "./js/app"],
     output: {
@@ -17,6 +18,11 @@ module.exports = {
             {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader"), exclude: /node_modules/},
             {test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader"), exclude: /node_modules/},
             {test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader"), exclude: /node_modules/},
+            {
+              test: /\.json$/,
+              loader: 'json-loader!' + path.resolve('loaders/json'),
+              exclude:/node_modules/
+            },
             {
                 test: /\.(png|jpg|ttf|eot|svg|woff)$/,
                 loader: 'url-loader?limit=6000&name=[name]-[hash].[ext]',
